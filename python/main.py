5f84d0461cd58ef273e2fc677e45dcc269969c2e
euaimonia <etown_2005@163.com>
2019-04-23 11:11:42 +0800

    初步完成功能

---
+++ python/main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, send_file
+import sys
+import hashlib
+import os
+
+file_info_list= []
+file_info_dict = {}
+
+def init():
+    file_list:str = sys.stdin.read().strip()
+    file_list:[str] = file_list.split('\n')
+    for file_name in file_list:
+        key = hashlib.md5(file_name.encode('utf-8')).hexdigest()
+        file_info_dict.setdefault(key, file_name)
+        file_info_list.append({
+            'key': key,
+            'file_name': file_name
+        })
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html', file_info_list = file_info_list)
+
+@app.route('/<file_hash>')
+def get_file(file_hash):
+    file_name = file_info_dict.get(file_hash)
+    if not file_name:
+        return 'file not found', 404
+    if not os.path.isfile(file_name):
+        return 'file not found', 404
+    return send_file(file_name)
+
+if __name__ == '__main__':
+    init()
+    print(file_info_list)
+    # debug = True时会导致__main__所在的进程反复重启
+    # 而程序参数信息会在重启的时候得不到保存而丢失
+    app.run('0.0.0.0', 8081, debug=False)
